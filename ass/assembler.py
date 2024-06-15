e5de984789a351d5153d1420468348b282e02a91
mac010ucsd <97704471+mac010ucsd@users.noreply.github.com>
2024-06-15 00:22:52 -0700

    REFACTOR EVERTYTHING!

--- ass/assembler.py
+++ ass/assembler.py
@@ -28,11 +28,11 @@ for i in range(len(ins_name)):
         opcode_bible[ins_name[i]] = {}
     j = op_format[i].strip().split("] ")
     j = [k.strip() for k in j]
-    print(j)
+    #print(j)
     opcode_bible[ins_name[i]][len(j)] = {
         "op_code": op_bits[i],
         "op_len": len(op_bits[i]),
-        "op_1": int(j[1][1:2]),
+        "op_1": 0 if len(j) <= 1 else int(j[1][1:2]),
         "op_2": 0 if len(j) <= 2 else int(j[2][1:2])
     }
 
@@ -45,21 +45,52 @@ while (oldfile != assfile):
 assfile = assfile.split("\n")
 
 outfile = ""
+
+jmp_book  = {}
+
+cnt = 0
+
+assfile_clean = []
+
 for i in assfile:
-    print(i)
-    i = i.split("//")[0].strip().split()
-    print(i)
+    p = i.split("//")
+    i = p[0].strip().split()
+    if len(p) == 2 and len(p[1].split()) == 2:
+        # then it is a label
+        jmp_book[cnt] = p[1].split()[1] 
+    
+    i = p[0].strip()
+    #print(i)
+    assfile_clean.append(i)
+    cnt += 1
+    
+assfile = assfile_clean
+
+jmp_decode = dict((v,k) for k,v in jmp_book.items())
+# print(assfile_clean)
+
+#print("jmp book")
+print(jmp_book)
+
+for i in assfile:
+    #print(i)
+    p = i.split("//")
+    i = p[0].strip().split()
+
+    #print(i)
     # print(i[0][len(i)])
     outline = ""
     outline += opcode_bible[i[0]][len(i)]["op_code"] # + " "
-    t = int(i[1]) if "reg" not in i[1] else int(str(i[1]).replace("reg", ""))
-    outline += f"{t:09b}"[8 - opcode_bible[i[0]][len(i)]["op_1"]:-1] # + " "
+    if len(i) > 1:
+        t = int(i[1]) if str(i[1]).isdigit() else (int(str(i[1]).replace("reg", "")) if "reg" in str(i[1]) else jmp_decode[str(i[1])])
+        outline += f"{t:09b}"[8 - opcode_bible[i[0]][len(i)]["op_1"]:-1] # + " "
     if len(i) > 2:
         t = int(i[2]) if "reg" not in i[2] else int(str(i[2]).replace("reg", ""))
         outline += f"{t:09b}"[8 - opcode_bible[i[0]][len(i)]["op_2"]:-1] # + " "
     outfile += outline + "\n"
+    cnt += 1
 
-print(outfile)
+# print(outfile)
 
 with open("output.txt", "w") as t:
     t.write(outfile)
