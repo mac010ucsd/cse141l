af4d6eba32c1915dbd4f46a0eaff43c48f1c65fe
Diyou Wang <dillwang47@gmail.com>
2024-06-14 20:38:27 -0700

    Fixed LUT for program 1

--- verilog/LUT.sv
+++ verilog/LUT.sv
@@ -7,16 +7,15 @@ always_comb case(LutPointer)
 	// Program 1
     0: absaddress = 14;
     1: absaddress = 23;
-    2: absaddress = 100;
-    3: absaddress = 106;
-    4: absaddress = 108;
-    5: absaddress = 110;
-	
+    2: absaddress = 44;
+    3: absaddress = 63;
+    4: absaddress = 75;
+    5: absaddress = 94;
+    6: absaddress = 100;
+    7: absaddress = 106;
+	8: absaddress = 108;
+    9: absaddress = 110;
 	// Program 2
-    6: absaddress = 1;
-    7: absaddress = 1;
-    8: absaddress = 1;
-    9: absaddress = 1;
     10: absaddress = 1;
     11: absaddress = 1;
     12: absaddress = 1;
@@ -25,7 +24,13 @@ always_comb case(LutPointer)
 
     // Program 3
     15: absaddress = 1;
-
+    16: absaddress = 1;
+    17: absaddress = 1;
+    18: absaddress = 1;
+    19: absaddress = 1;
+    20: absaddress = 1;
+    21: absaddress = 1;
+    22: absaddress = 1;
 	
 
 	default: absaddress = 'b0;
